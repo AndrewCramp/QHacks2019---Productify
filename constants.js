d6ed69387fa5a919fc84af83cece628c9598108d
nicoleooi <40377361+nicoleooi@users.noreply.github.com>
2019-02-02 23:06:04 -0500

    Added files

---
+++ constants.js
@@ -0,0 +1,117 @@
+//this file will hold all the achievement data
+
+var productivity = {
+  "snail" : {
+    "picUrl" : "images/snail.png",
+    "achieved" : "yes",
+    "inprogress" : "yes",
+    "hours" : "1"
+  },
+  "turtle" : {
+    "picUrl" : "images/turtle.png",
+    "achieved" : "no",
+    "inprogress" : "no",
+    "hours" : "4"
+  },
+  "bike" : {
+    "picUrl" : "images/bike.png",
+    "achieved" : "no",
+    "inprogress" : "no",
+    "hours" : "16"
+  },
+  "car" : {
+    "picUrl" : "images/car.png",
+    "achieved" : "no",
+    "inprogress" : "no",
+    "hours" : "256"
+  },
+  "train" : {
+    "picUrl" : "images/train.png",
+    "achieved" : "no",
+    "inprogress" : "no",
+    "hours" : "65536"
+  },
+  "airplane" : {
+    "picUrl" : "images/airplane.png",
+    "achieved" : "no",
+    "inprogress" : "no",
+    "hours" : "4000000"
+  }
+}
+
+var reading = {
+  "paperclip" : {
+    "picUrl" : "images/paperclip.png",
+    "achieved" : "no",
+    "inprogress" : "no",
+    "hours" : "1"
+  },
+  "pencil" : {
+    "picUrl" : "images/pencil.png",
+    "achieved" : "no",
+    "inprogress" : "no",
+    "hours" : "4"
+  },
+  "book" : {
+    "picUrl" : "images/book.png",
+    "achieved" : "no",
+    "hours" : "16"
+  },
+  "bookstack" : {
+    "picUrl" : "images/bookstack.png",
+    "achieved" : "no",
+    "inprogress" : "no",
+    "hours" : "256"
+  },
+  "bar" : {
+    "picUrl" : "images/bargraph.png",
+    "achieved" : "no",
+    "inprogress" : "no",
+    "hours" : "65536"
+  },
+  "world" : {
+    "picUrl" : "images/world.png",
+    "achieved" : "no",
+    "inprogress" : "no",
+    "hours" : "4000000"
+  }
+}
+
+var social = {
+  "shock" : {
+    "picUrl" : "images/shock.png",
+    "achieved" : "no",
+    "inprogress" : "no",
+    "hours" : "1"
+  },
+  "gritted" : {
+    "picUrl" : "images/gritted.png",
+    "achieved" : "no",
+    "inprogress" : "no",
+    "hours" : "4"
+  },
+  "guilt" : {
+    "picUrl" : "images/guilt.png",
+    "achieved" : "no",
+    "inprogress" : "no",
+    "hours" : "16"
+  },
+  "sobbing" : {
+    "picUrl" : "images/sobbing.png",
+    "achieved" : "no",
+    "inprogress" : "no",
+    "hours" : "256"
+  },
+  "supershock" : {
+    "picUrl" : "images/supershock.png",
+    "achieved" : "no",
+    "inprogress" : "no",
+    "hours" : "65536"
+  },
+  "poo" : {
+    "picUrl" : "images/world.png",
+    "achieved" : "no",
+    "inprogress" : "no",
+    "hours" : "poo"
+  }
+}
